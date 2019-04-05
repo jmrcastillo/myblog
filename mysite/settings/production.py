74a6a494e5368d4728148bbf17ed6792ffc150e1
jmrcastillo <jm.cass20@gmail.com>
2019-04-05 20:50:32 +0000

    comment database

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -11,15 +11,15 @@ SECRET_KEY = 'n6$gz-vsdfadf84=@((@%%s*ak@wp!c)bxk$w3)js-0fhogp0m(#xab$'
 
 ALLOWED_HOSTS = ['myblog-jmr.herokuapp.com']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog',
-        'USER': 'postgres',
-        'PASSWORD': 'ichoose',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'blog',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'ichoose',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432'
+    # }
+# }
 
 django_heroku.settings(locals())
